2221ea701b67f22159fd5d40d329235f4dba0a78
Daniel <Dcisneros@unicauca.edu.co>
2025-09-18 16:11:11 -0500

    cambio enum estadosSolicitud

--- gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/dominio/modelos/Enums/EstadosSolicitud.java
+++ gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/dominio/modelos/Enums/EstadosSolicitud.java
@@ -1,13 +1,10 @@
 package co.edu.unicauca.decanatura.gestion_curricular.dominio.modelos.Enums;
 
 public enum EstadosSolicitud {
-    Aprobada,
-    Rechazada,
-    Enviada,
-    EN_REVISION_SECRETARIO,
-    APROBADA_POR_SECRETARIO,
-    RECHAZADA_POR_SECRETARIO,
-    PENDIENTE_COORDINADOR,
-    APROBADA_POR_COORDINADOR,
-    RECHAZADA_POR_COORDINADOR
+    ENVIADA,
+    EN_REVISION_SECRETARIA,
+    EN_REVISION_FUNCIONARIO ,
+    EN_REVISION_COORDINADOR,
+    APROBADA ,
+    RECHAZADA
 }
