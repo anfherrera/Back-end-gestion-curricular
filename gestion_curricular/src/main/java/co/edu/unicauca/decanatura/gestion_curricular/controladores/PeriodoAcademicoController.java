a4771331018fe2f32f1ae17f6e63e88566337e5b
Andrés Felipe Herrera <128004097+anfherrera@users.noreply.github.com>
2025-10-27 10:05:13 -0500

    fix: Corregir configuraciÃ³n CORS en controladores PeriodoAcademico y TipoDocumento

--- gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/controladores/PeriodoAcademicoController.java
+++ gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/controladores/PeriodoAcademicoController.java
@@ -15,7 +15,6 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api/periodos-academicos")
-@CrossOrigin(origins = "*")
 public class PeriodoAcademicoController {
 
     /**
--- gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/controladores/TipoDocumentoController.java
+++ gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/controladores/TipoDocumentoController.java
@@ -15,7 +15,6 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api/tipos-documento")
-@CrossOrigin(origins = "*")
 public class TipoDocumentoController {
 
     /**
