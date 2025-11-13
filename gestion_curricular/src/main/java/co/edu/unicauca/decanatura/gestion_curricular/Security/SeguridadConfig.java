d7ebf4287ef30adda1f03c48c7f4d80e03316248
Andrés Felipe Herrera <128004097+anfherrera@users.noreply.github.com>
2025-11-13 18:52:13 -0500

    feat: Add CORS configuration to SeguridadConfig for enhanced security
    - Introduced a new CORS configuration to allow specific origins and methods, improving the security and flexibility of the application.
    - Configured allowed origins for local and production environments, ensuring proper access control for cross-origin requests.

--- gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/Security/SeguridadConfig.java
+++ gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/Security/SeguridadConfig.java
@@ -1,5 +1,7 @@
 package co.edu.unicauca.decanatura.gestion_curricular.Security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +44,22 @@ public class SeguridadConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200",
+            "https://front-end-gestion-curricular.vercel.app"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
