678428b24938118ff705010370395c1ced1a6fe9
Andrés Felipe Herrera <128004097+anfherrera@users.noreply.github.com>
2025-12-06 12:36:26 -0500

    r

--- gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/GestionCurricularApplication.java
+++ gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/GestionCurricularApplication.java
@@ -5,16 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * Clase principal de la aplicación de Gestión Curricular
- */
+
 @SpringBootApplication
 @ComponentScan(basePackages = "co.edu.unicauca.decanatura")
 @EnableCaching
 public class GestionCurricularApplication {
 
 	public static void main(String[] args) {
-		// Iniciar aplicación Spring Boot
 		SpringApplication.run(GestionCurricularApplication.class, args);
 	}
 }
