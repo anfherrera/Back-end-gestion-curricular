b34948c5dde61ac7c4b2961a2cfa1597ef327b9d
Andrés Felipe Herrera <128004097+anfherrera@users.noreply.github.com>
2025-12-17 12:59:13 -0500

    refactor: eliminar código comentado en GestionarSolicitudGatewayImplAdapter
    - Se eliminaron bloques de código comentado en `GestionarSolicitudGatewayImplAdapter`, simplificando la implementación y mejorando la legibilidad del código.
    - Esta limpieza no afecta la funcionalidad existente, manteniendo el comportamiento de la aplicación intacto.

--- gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/infraestructura/output/persistencia/gateway/GestionarSolicitudGatewayImplAdapter.java
+++ gestion_curricular/src/main/java/co/edu/unicauca/decanatura/gestion_curricular/infraestructura/output/persistencia/gateway/GestionarSolicitudGatewayImplAdapter.java
@@ -50,50 +50,6 @@ public class GestionarSolicitudGatewayImplAdapter implements GestionarSolicitudG
         this.solicitudMapper = solicitudMapper;
     }
 
-    // @Override
-    // @Transactional
-    // public SolicitudCursoVeranoIncripcion crearSolicitudCursoVeranoInscripcion(
-    //        SolicitudCursoVeranoIncripcion solicitudCursoVerano) {
-    //    SolicitudCursoVeranoInscripcionEntity solicitudCursoVeranoEntity = solicitudMapper.map(solicitudCursoVerano, SolicitudCursoVeranoInscripcionEntity.class);
-    //    solicitudCursoVeranoEntity.setNombre_solicitud(SolicitudCursoVeranoIncripcion.class.getSimpleName());
-    //    solicitudCursoVeranoEntity.setFecha_registro_solicitud(new Date());
-    //    EstadoSolicitudEntity estadoSolicitudEntity = null;
-        
-    //    estadoSolicitudEntity = new EstadoSolicitudEntity();
-    //    estadoSolicitudEntity.setFecha_registro_estado(new Date());
-    //    estadoSolicitudEntity.setObjSolicitud(solicitudCursoVeranoEntity);
-        
-    //    List<EstadoSolicitudEntity> estadosSolcitud = solicitudCursoVeranoEntity.getEstadosSolicitud();
-    //    estadosSolcitud.add(estadoSolicitudEntity);
-    //    solicitudCursoVeranoEntity.setEstadosSolicitud(estadosSolcitud);
-    //    CursoOfertadoVerano cursoOfertado = solicitudCursoVerano.getObjCursoOfertadoVerano();
-    //    CursoOfertadoVeranoEntity cursoOfertadoVeranoEntity = null;
-    //    if(cursoOfertado != null && cursoOfertado.getId_curso() != null) {
-    //        Integer idCurso = cursoOfertado.getId_curso();
-    //        if(cursoOfertadoVeranoRepository.existsById(idCurso)){
-    //            cursoOfertadoVeranoEntity = cursoOfertadoVeranoRepository.findById(idCurso)
-    //                    .orElseThrow(() -> new IllegalArgumentException("Curso ofertado no encontrado con ID: " + idCurso));
-    //        } else {
-    //            cursoOfertadoVeranoEntity = new CursoOfertadoVeranoEntity();
-    //            GrupoCursoVeranoEntity  grupoEntity = solicitudMapper.map(cursoOfertado.getGrupo(), GrupoCursoVeranoEntity.class);
-    //            cursoOfertadoVeranoEntity.setGrupo(grupoEntity);
-    //            cursoOfertadoVeranoEntity.setCupo_estimado(cursoOfertado.getCupo_estimado());
-    //            cursoOfertadoVeranoEntity.setSalon(cursoOfertado.getSalon());
-    //            MateriaEntity materiaEntity = solicitudMapper.map(cursoOfertado.getObjMateria(), MateriaEntity.class);
-    //            DocenteEntity docenteEntity = solicitudMapper.map(cursoOfertado.getObjDocente(), DocenteEntity.class);
-    //            cursoOfertadoVeranoEntity.setObjMateria(materiaEntity);
-    //            cursoOfertadoVeranoEntity.setObjDocente(docenteEntity);
-                
-    //        }
-    //        solicitudCursoVeranoEntity.setObjCursoOfertadoVerano(cursoOfertadoVeranoEntity);
-    //    } else {
-    //        throw new IllegalArgumentException("El curso ofertado no puede ser nulo o debe tener un ID válido.");
-    //    }
-    //    SolicitudCursoVeranoInscripcionEntity solicitudCursoVeranoGuardado = solicitudRepository.save(solicitudCursoVeranoEntity);
-
-    //    return solicitudMapper.map(solicitudCursoVeranoGuardado, SolicitudCursoVeranoIncripcion.class); // Implementación pendiente
-    // }
-
     @Override
     @Transactional
     public SolicitudEcaes crearSolicitudEcaes(SolicitudEcaes solicitudEcaes) {
