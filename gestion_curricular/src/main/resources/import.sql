7b46617485c8a0d7c2d46ebb3fa7190a97bca047
Daniel <Dcisneros@unicauca.edu.co>
2026-02-17 00:43:28 -0500

    añadir imports necesarios

--- gestion_curricular/src/main/resources/import.sql
+++ gestion_curricular/src/main/resources/import.sql
@@ -123,6 +123,10 @@ INSERT INTO Usuarios(idUsuario, codigo, nombre_completo, correo, password, estad
 -- Password: password123 (hash BCrypt)
 -- IMPORTANTE: Cambiar la contraseña en producción
 INSERT INTO Usuarios(idUsuario, codigo, nombre_completo, correo, password, estado_usuario, idfkRol, idfkPrograma, cedula) VALUES (26, 'ADMIN001', 'Administrador del Sistema', 'admin@unicauca.edu.co', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjFOjtXgjOU92LhL6YP1dPW1Z/uLlLG', 1, 1, 1, '0000000000');
+INSERT INTO Usuarios(idUsuario, codigo, nombre_completo, correo, password, estado_usuario, idfkRol, idfkPrograma, cedula) VALUES (27, '1046190112', 'Daniel Cisneros', 'dcisneros@unicauca.edu.co', '$2a$12$kPCRQb320eqFwn08XBFDkuJYICOG7H5ou08fmHQ3.zaKZjAtGyI9G', 1, 2, 3, '100791411');
+INSERT INTO Usuarios(idUsuario, codigo, nombre_completo, correo, password, estado_usuario, idfkRol, idfkPrograma, cedula) VALUES (28, '55555555', 'Juan Pablo', 'juanp@unicauca.edu.co', '$2a$12$hnaM9zNGpcYBN6FEWuECjO7Eh.rJ9dXPInXbt8sz9dedJp8zxniYi', 1, 3, 3, '55555555');
+INSERT INTO Usuarios(idUsuario, codigo, nombre_completo, correo, password, estado_usuario, idfkRol, idfkPrograma, cedula) VALUES (29, '10293838', 'josefina rodriguez', 'josefina@unicauca.edu.co', '$2a$12$zHDPzonzaTYw/SDvqZaReehyTo9ri51vg5N1C9DaYHIdeZi9PVQUG', 1, 3, 3, '10293838');
+INSERT INTO Usuarios(idUsuario, codigo, nombre_completo, correo, password, estado_usuario, idfkRol, idfkPrograma, cedula) VALUES (30, '99009876', 'Maria flor', 'maria@unicauca.edu.co', '$2a$12$IterW17WKe75BkVsMSU9SuFb.DcAB6diCk8Ef8fxwzQ7nvdLJyBr.', 1, 4, 3, '0000000000');
 
 -- Solo insertar cursos adicionales (el curso 1 ya existe)
 -- Agregar periodo_academico a los cursos
